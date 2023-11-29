29fe0b76765a80c660e6ad724e469065705d8778
Laion Corcino <laioncorcino@gmail.com>
2023-11-29 00:25:06 -0300

    [test] valid entity product

---
+++ internal/entity/product_test.go
@@ -0,0 +1,35 @@
+package entity
+
+import (
+	"github.com/laioncorcino/go-product-user/pkg"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewProduct(t *testing.T) {
+	p, err := NewProduct("Product 1", 10)
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.NotEmpty(t, p.ProductID)
+	assert.True(t, pkg.IsUUID(p.ProductID))
+	assert.Equal(t, p.Name, "Product 1")
+	assert.Equal(t, p.Price, 10)
+}
+
+func TestProduct_ValidateNameIsRequired(t *testing.T) {
+	p, err := NewProduct("", 10)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrNameIsRequired, err)
+}
+
+func TestProduct_ValidatePriceIsRequired(t *testing.T) {
+	p, err := NewProduct("Product 1", 0)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrPriceIsRequired, err)
+}
+
+func TestProduct_ValidatePriceIsInvalid(t *testing.T) {
+	p, err := NewProduct("Product 1", -10)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrInvalidPrice, err)
+}
